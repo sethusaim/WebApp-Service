aeecbce029422a135322cc89d6116d8b68297ad0
sethusaim <sethusaim@gmail.com>
2022-05-25 10:55:31 +0530

    updated requirements.txt

--- setup.sh
+++ setup.sh
@@ -4,3 +4,17 @@ sudo apt update
 
 sudo apt-get update
 
+sudo apt install python3-pip
+
+git init 
+
+git remote remove origin
+
+git remote add origin https://github.com/sethusaim/WebApp-Service.git
+
+git fetch origin
+
+git checkout origin/main
+
+pip3 install -r requirements.txt
+
